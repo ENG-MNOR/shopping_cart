fd77a74d9bf3c4da74468708df9523d3ccee0cf4
mohamednor <mohamednorboorow@gmail.com>
2024-02-28 21:46:58 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../style.css'
+
+
+const Header=()=> {
+  
+  return (
+    <div className="menu" >
+        <a href="">logo</a>
+        <ul className='menu-items'>
+        <li>
+            <a href="">Home</a>
+        </li>
+        <li>
+            <a href="">About</a>
+        </li>  <li>
+            <a href="">Cart</a>
+        </li>  <li>
+            <a href="">Contact</a>
+        </li>
+        </ul>
+        <a href="">5</a>
+     
+    </div>
+  )
+}
+
+export default Header
